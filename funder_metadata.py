3405d89e2356339ef36bbfa2ab9468ad7eac70db
Krystal Hahn <krystalmhahn@gmail.com>
2025-12-03 16:48:51 -0800

    Add progress bar and completion message

--- funder_metadata.py
+++ funder_metadata.py
@@ -1,15 +1,19 @@
-# MattF's original script for funder_metadata
-
 def generate_funder_metadata_csv():
     import csv
     import io
+    from tqdm import tqdm
     from osf.utils.outcomes import ArtifactTypes
-    filename = '/tmp/funder_metadata.csv'
+
+    filename = f'/tmp/funder_metadata.csv'
     COL_HEADERS = ['funder', 'type', 'resourceType', 'guid', 'title', 'created', 'subjects', 'institutions', 'hasConnectedResource', 'visible_contributors']
     output = io.StringIO()
     writer = csv.DictWriter(output, COL_HEADERS)
     writer.writeheader()
+
     qs = GuidMetadataRecord.objects.exclude(funding_info=[])
+
+    pbar = tqdm(total=qs.count())
+
     for gmr in qs:
         ref = gmr.guid.referent
         if hasattr(ref, 'is_public') and not ref.is_public:
@@ -18,10 +22,12 @@ def generate_funder_metadata_csv():
             continue
         if 'file' in ref.type and not ref.target.is_public:
             continue
+
         idents = ref.identifiers.all() if not 'file' in ref.type else ref.target.identifiers.all()
         partifacts = sum([list(i.artifact_metadata.filter(artifact_type=ArtifactTypes.PRIMARY.value)) for i in idents], [])
         outcomes = [pa.outcome for pa in partifacts]
         has_artifacts = any([o.artifact_metadata.exclude(artifact_type=ArtifactTypes.PRIMARY.value).filter(finalized=True, deleted__isnull=True).exists() for o in outcomes])
+
         for fund_dict in gmr.funding_info:
             writer.writerow({
                 'funder': fund_dict['funder_name'],
@@ -35,5 +41,11 @@ def generate_funder_metadata_csv():
                 'hasConnectedResource': has_artifacts,
                 'visible_contributors': list(ref.contributor_set.filter(visible=True).values_list('user__guids___id', flat=True)) if hasattr(ref, 'contributor_set') else [],
             })
+        
+        pbar.update(1)
+    pbar.close()
+    
     with open(filename, 'w') as writeFile:
         writeFile.write(output.getvalue())
+
+    print(f"Output written to {filename}")
