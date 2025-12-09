26703a01462ecc295a54fae9ef2482834ff7077a
Krystal Hahn <krystalmhahn@gmail.com>
2025-12-09 15:38:56 -0800

    Add storage region to script

--- ad-hoc/user_creations_by_domain.py
+++ ad-hoc/user_creations_by_domain.py
@@ -1,12 +1,12 @@
 def fetch_user_creations_by_domain(ds):
-    from osf.metrics import CountedAuthUsage
     import csv
     import io
     filename = '/tmp/user_creations_by_domain.csv'
-    COL_HEADERS = [ 'user_domain', 'user_subdomain', 'user_guid', 'object_guid', 'object_type', 'object_title', 'object_created']
+    COL_HEADERS = [ 'user_domain', 'user_subdomain', 'user_guid', 'object_guid', 'object_type', 'object_title', 'object_created', 'object_storage_region']
     output = io.StringIO()
     writer = csv.DictWriter(output, COL_HEADERS)
     writer.writeheader()
+
     for d in ds:
         target_users = OSFUser.objects.exclude(spam_status__in=[1,2]).filter(is_active=True, emails__address__endswith=d)
         for u in target_users.distinct():
@@ -21,7 +21,8 @@ def fetch_user_creations_by_domain(ds):
                     'object_guid': n._id,
                     'object_type': n.type,
                     'object_title': n.title,
-                    'object_created': n.created
+                    'object_created': n.created,
+                    'object_storage_region': n.addons_osfstorage_node_settings.region.name
                 })
             for p in u.preprints_created.filter(is_public=True, is_published=True):
                 writer.writerow({
@@ -31,7 +32,10 @@ def fetch_user_creations_by_domain(ds):
                     'object_guid': p._id,
                     'object_type': 'osf.preprint',
                     'object_title': p.title,
-                    'object_created': p.created
+                    'object_created': p.created,
+                    'object_storage_region': p.region.name
                 })
     with open(filename, 'w') as writeFile:
         writeFile.write(output.getvalue())
+
+    print(f"Output written to {filename}")
