76a69bee020b467717ec3c49054fcfeaa4ff4af6
Krystal Hahn <krystalmhahn@gmail.com>
2025-12-15 18:23:29 -0800

    Differentiate between all and non-deleted nodes

--- quarterly/regional_storage_metrics.py
+++ quarterly/regional_storage_metrics.py
@@ -1,3 +1,4 @@
+# Differentiating between all nodes and non-deleted nodes (resolves discrepancy)
 # Total, not only for the quarter
 def get_regional_storage_metrics_for_insts(iids=None):
     import csv
@@ -5,7 +6,7 @@ def get_regional_storage_metrics_for_insts(iids=None):
     from tqdm import tqdm
 
     filename = f'/tmp/regional_storage_metrics.csv'
-    COL_HEADERS = ['institution.name', 'region.name', 'public_nodes', 'private_nodes', 'public_storage', 'private_storage']
+    COL_HEADERS = ['institution.name', 'region.name', 'public_nodes_all', 'private_nodes_all', 'public_storage_all', 'private_storage_all', 'public_nodes', 'private_nodes', 'public_storage', 'private_storage']
     output = io.StringIO()
     writer = csv.DictWriter(output, COL_HEADERS)
     writer.writeheader()
@@ -32,17 +33,23 @@ def get_regional_storage_metrics_for_insts(iids=None):
             domain_metrics = {
                 'institution.name': i.name,
                 'region.name': r.name,
-                'public_nodes': ns.filter(is_public=True).count(),
-                'private_nodes': ns.filter(is_public=False).count(),
-                'public_storage': sum([sum([s for s in n.files.values_list('versions__size', flat=True) if isinstance(s, int)]) for n in ns.filter(is_public=True)]),
-                'private_storage': sum([sum([s for s in n.files.values_list('versions__size', flat=True) if isinstance(s, int)]) for n in ns.filter(is_public=False)])
+                'public_nodes_all': ns.filter(is_public=True).count(),
+                'private_nodes_all': ns.filter(is_public=False).count(),
+                'public_storage_all': sum([sum([s for s in n.files.values_list('versions__size', flat=True) if isinstance(s, int)]) for n in ns.filter(is_public=True)]),
+                'private_storage_all': sum([sum([s for s in n.files.values_list('versions__size', flat=True) if isinstance(s, int)]) for n in ns.filter(is_public=False)]),
+                # excluding deleted
+                'public_nodes': ns.filter(is_public=True, deleted__isnull=True).count(),
+                'private_nodes': ns.filter(is_public=False, deleted__isnull=True).count(),
+                'public_storage': sum([sum([s for s in n.files.values_list('versions__size', flat=True) if isinstance(s, int)]) for n in ns.filter(is_public=True, deleted__isnull=True)]),
+                'private_storage': sum([sum([s for s in n.files.values_list('versions__size', flat=True) if isinstance(s, int)]) for n in ns.filter(is_public=False, deleted__isnull=True)])
             }
-            if domain_metrics.get('public_nodes', 0) or domain_metrics.get('private_nodes', 0):
+            # skip if public_nodes_all and private_nodes_all are 0, in case there are deleted nodes in a regions
+            if domain_metrics.get('public_nodes_all', 0) or domain_metrics.get('private_nodes_all', 0):
                 writer.writerow(domain_metrics)
         pbar.update(1)
 
     pbar.close()
-
+    
     with open(filename, 'w') as writeFile:
         writeFile.write(output.getvalue())
 
