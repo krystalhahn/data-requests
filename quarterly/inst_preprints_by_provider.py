6cfbcf49ac5b685e60d6f38eeb11fbbbfd0b024c
Krystal Hahn <krystalmhahn@gmail.com>
2025-12-16 14:14:40 -0800

    Remove row printing and add progress bar and completion message

--- quarterly/inst_preprints_by_provider.py
+++ quarterly/inst_preprints_by_provider.py
@@ -1,21 +1,27 @@
 # Total, not only for the quarter
-def inst_preprints_by_provider():
+def get_inst_preprints_by_provider():
     import csv
     import io
-    from django.utils import timezone
-    import pytz
+    from tqdm import tqdm
+
     filename = f'/tmp/inst_preprintprovider_metrics.csv'
     COL_HEADERS = ['institution.name', 'provider.name', 'total_preprints']
     output = io.StringIO()
     writer = csv.DictWriter(output, COL_HEADERS)
     writer.writeheader()
+
     insts = Institution.objects.all()
+
+    pbar = tqdm(total=insts.count())
+
     for i in insts:
         for ppp in PreprintProvider.objects.all():
+
             users = OSFUser.objects.filter(
                 institutionaffiliation__institution__id=i.id,
                 is_active=True
             ).exclude(spam_status=2).distinct()
+
             domain_metrics = {
                 'institution.name': i.name,
                 'provider.name': ppp.name,
@@ -23,6 +29,12 @@ def inst_preprints_by_provider():
             }
             if domain_metrics.get('total_preprints', 0):
                 writer.writerow(domain_metrics)
-                print(domain_metrics)
+
+        pbar.update(1)
+
+    pbar.close()
+
     with open(filename, 'w') as writeFile:
         writeFile.write(output.getvalue())
+
+    print(f"Output written to {filename}")
