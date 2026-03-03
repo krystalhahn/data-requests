08a58c2b2e9b44e6dd1c71e0806b0d284ca8f4e9
Krystal Hahn <krystalmhahn@gmail.com>
2026-03-02 16:51:14 -0800

    Incorporate funders represented in registration metadata

--- monthly/los_registrations.py
+++ monthly/los_registrations.py
@@ -28,16 +28,20 @@ def get_all_registrations_for_los():
         outcomes = [pa.outcome for pa in partifacts]
 
         connected_resources = []
-
         ARTIFACT_TYPE_LABELS = dict(ArtifactTypes.choices())
         for o in outcomes:
             connected_artifacts = o.artifact_metadata.exclude(
                 artifact_type=ArtifactTypes.PRIMARY.value
             ).filter(finalized=True, deleted__isnull=True)
-        
             for artifact in connected_artifacts:
                 artifact_label = ARTIFACT_TYPE_LABELS.get(artifact.artifact_type, str(artifact.artifact_type))
                 connected_resources.append(artifact_label)
+
+        gmr = GuidMetadataRecord.objects.filter(guid___id=reg._id).first()
+        funding_info = gmr.funding_info if gmr else []
+        funders = [f['funder_name'] for f in funding_info] or None
+        funder_identifiers = [f['funder_identifier'] for f in funding_info] or None
+        funder_identifier_types = [f['funder_identifier_type'] for f in funding_info] or None
 
         writer.writerow({
             'reg_guid': reg._id,
@@ -54,6 +58,9 @@ def get_all_registrations_for_los():
             'institution': list(reg.affiliated_institutions.values_list('name', flat=True)) if hasattr(reg, 'affiliated_institutions') else [],
             'subject': list(reg.subjects.filter(parent_id__isnull=False).values_list('text', flat=True)) if hasattr(reg, 'subjects') else [],
             'subject_parent': list(reg.subjects.filter(parent_id__isnull=True).values_list('text', flat=True)) if hasattr(reg, 'subjects') else [],
+            'funder': funders,
+            'funder_identifier': funder_identifiers,
+            'funder_identifier_type': funder_identifier_types,
         })
         pbar.update()
 
