0e3d28421db37ad56ba69de99630e5e8a71cb066
Krystal Hahn <krystalmhahn@gmail.com>
2026-03-17 16:44:51 -0700

    Reorder columns to reflect progression to LOS

--- monthly/los_metrics/los_metrics_sheet.R
+++ monthly/los_metrics/los_metrics_sheet.R
@@ -27,12 +27,12 @@ pct <- function(x) round(mean(x) * 100, 1)
 ### Overall ----
 public_reg_overall <- public_reg %>%
   summarize(total = n(),
-            LOS_n = sum(is_los),
-            LOS_pct = pct(is_los),
             outputs_n = sum(has_output),
             outputs_pct = pct(has_output),
             outcomes_n = sum(has_outcome),
-            outcomes_pct = pct(has_outcome))
+            outcomes_pct = pct(has_outcome),
+            LOS_n = sum(is_los),
+            LOS_pct = pct(is_los))
 
 write_sheet(public_reg_overall, los_sheet_url, "Overall")
 
@@ -46,12 +46,12 @@ public_reg_affiliated <- public_reg %>%
   })) %>%
   group_by(affiliated) %>%
   summarize(total = n(),
-            LOS_n = sum(is_los),
-            LOS_pct = pct(is_los),
             outputs_n = sum(has_output),
             outputs_pct = pct(has_output),
             outcomes_n = sum(has_outcome),
-            outcomes_pct = pct(has_outcome))
+            outcomes_pct = pct(has_outcome),
+            LOS_n = sum(is_los),
+            LOS_pct = pct(is_los))
 
 write_sheet(public_reg_affiliated, los_sheet_url, "Affiliated")
 
@@ -67,12 +67,12 @@ public_reg_institution <- public_reg %>%
   mutate(institution = if_else(is.na(institution), "Unaffiliated", institution)) %>%
   group_by(institution) %>%
   summarize(total = n(),
-            LOS_n = sum(is_los),
-            LOS_pct = pct(is_los),
             outputs_n = sum(has_output),
             outputs_pct = pct(has_output),
             outcomes_n = sum(has_outcome),
-            outcomes_pct = pct(has_outcome))
+            outcomes_pct = pct(has_outcome),
+            LOS_n = sum(is_los),
+            LOS_pct = pct(is_los))
 
 write_sheet(public_reg_institution, los_sheet_url, "Institution")
 
@@ -86,12 +86,12 @@ public_reg_funded <- public_reg %>%
   })) %>%
   group_by(funded) %>%
   summarize(total = n(),
-            LOS_n = sum(is_los),
-            LOS_pct = pct(is_los),
             outputs_n = sum(has_output),
             outputs_pct = pct(has_output),
             outcomes_n = sum(has_outcome),
-            outcomes_pct = pct(has_outcome))
+            outcomes_pct = pct(has_outcome),
+            LOS_n = sum(is_los),
+            LOS_pct = pct(is_los))
 
 write_sheet(public_reg_funded, los_sheet_url, "Funded")
 
@@ -107,12 +107,12 @@ public_reg_funder <- public_reg %>%
   mutate(funder = if_else(is.na(funder), "Unfunded", funder)) %>%
   group_by(funder) %>%
   summarize(total = n(),
-            LOS_n = sum(is_los),
-            LOS_pct = pct(is_los),
             outputs_n = sum(has_output),
             outputs_pct = pct(has_output),
             outcomes_n = sum(has_outcome),
-            outcomes_pct = pct(has_outcome))
+            outcomes_pct = pct(has_outcome),
+            LOS_n = sum(is_los),
+            LOS_pct = pct(is_los))
 
 write_sheet(public_reg_funder, los_sheet_url, "Funder")
 
@@ -121,12 +121,12 @@ write_sheet(public_reg_funder, los_sheet_url, "Funder")
 public_reg_template <- public_reg %>%
   group_by(template) %>%
   summarize(total = n(),
-            LOS_n = sum(is_los),
-            LOS_pct = pct(is_los),
             outputs_n = sum(has_output),
             outputs_pct = pct(has_output),
             outcomes_n = sum(has_outcome),
-            outcomes_pct = pct(has_outcome))
+            outcomes_pct = pct(has_outcome),
+            LOS_n = sum(is_los),
+            LOS_pct = pct(is_los))
 
 write_sheet(public_reg_template, los_sheet_url, "Template")
 
@@ -135,12 +135,12 @@ write_sheet(public_reg_template, los_sheet_url, "Template")
 public_reg_registry <- public_reg %>%
   group_by(registry) %>%
   summarize(total = n(),
-            LOS_n = sum(is_los),
-            LOS_pct = pct(is_los),
             outputs_n = sum(has_output),
             outputs_pct = pct(has_output),
             outcomes_n = sum(has_outcome),
-            outcomes_pct = pct(has_outcome))
+            outcomes_pct = pct(has_outcome),
+            LOS_n = sum(is_los),
+            LOS_pct = pct(is_los))
 
 write_sheet(public_reg_registry, los_sheet_url, "Registry")
 
@@ -156,12 +156,12 @@ public_reg_subject_parent <- public_reg %>%
   mutate(subject_parent = if_else(is.na(subject_parent), "No subject", subject_parent)) %>%
   group_by(subject_parent) %>%
   summarize(total = n(),
-            LOS_n = sum(is_los),
-            LOS_pct = pct(is_los),
             outputs_n = sum(has_output),
             outputs_pct = pct(has_output),
             outcomes_n = sum(has_outcome),
-            outcomes_pct = pct(has_outcome))
+            outcomes_pct = pct(has_outcome),
+            LOS_n = sum(is_los),
+            LOS_pct = pct(is_los))
 
 write_sheet(public_reg_subject_parent, los_sheet_url, "Top-level Subject")
 
@@ -176,12 +176,12 @@ public_reg_subject <- public_reg %>%
   mutate(subject = if_else(is.na(subject), "No subject", subject)) %>%
   group_by(subject) %>%
   summarize(total = n(),
-            LOS_n = sum(is_los),
-            LOS_pct = pct(is_los),
             outputs_n = sum(has_output),
             outputs_pct = pct(has_output),
             outcomes_n = sum(has_outcome),
-            outcomes_pct = pct(has_outcome))
+            outcomes_pct = pct(has_outcome),
+            LOS_n = sum(is_los),
+            LOS_pct = pct(is_los))
 
 write_sheet(public_reg_subject, los_sheet_url, "Lower-level subject")
 
