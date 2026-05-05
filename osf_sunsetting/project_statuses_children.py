0d4e7e52cfab11f574c9f5ee15d977e69ca950c1
Krystal Hahn <krystalmhahn@gmail.com>
2026-05-04 17:32:11 -0700

    Initial commit of script pulling nodes, their privacy status, and counts of public/private child nodes

---
+++ osf_sunsetting/project_statuses_children.py
@@ -0,0 +1,33 @@
+def get_project_statuses_children():
+    import csv
+    import io
+    from django.db.models import Q, Count
+    from tqdm import tqdm
+
+    filename = "/tmp/project_statuses_children.csv"
+    COL_HEADERS = ['node_guid', 'is_public', 'private_children', 'public_children']
+    output = io.StringIO()
+    writer = csv.DictWriter(output, COL_HEADERS)
+    writer.writeheader()
+
+    nodes = Node.objects.annotate(
+        private_children=Count('descendants', filter=Q(descendants__is_public=False)),
+        public_children=Count('descendants', filter=Q(descendants__is_public=True))
+    ).prefetch_related(None)
+
+    pbar = tqdm(total = nodes.count())
+
+    for n in nodes.iterator():
+        writer.writerow({
+            'node_guid': n._id,
+            'is_public': "public" if n.is_public else "private",
+            'private_children': n.private_children,
+            'public_children': n.public_children
+        })
+        pbar.update()
+
+    pbar.close()
+    with open(filename, 'w') as writeFile:
+        writeFile.write(output.getvalue())
+
+    print(f"Output written to {filename}")
