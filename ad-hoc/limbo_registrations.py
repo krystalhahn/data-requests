d7b7dc152bd3a018fb4e97a38fc7b96b388aedfd
Krystal Hahn <krystalmhahn@gmail.com>
2026-07-06 15:18:55 -0700

    Add registration_approval_state field

--- ad-hoc/limbo_registrations.py
+++ ad-hoc/limbo_registrations.py
@@ -19,12 +19,12 @@ def get_limbo_registrations():
 
     filename = '/tmp/limbo_registrations.csv'
     COL_HEADERS = ['reg_guid', 'is_public', 'is_deleted', 'spam_status', 'date_created', 'date_registered', 
-                   'moderation_state', 'embargo_state', 'embargo_end_date', 'retraction_state']
+                   'moderation_state', 'registration_approval_state', 'embargo_state', 'embargo_end_date', 'retraction_state']
     output = io.StringIO()
     writer = csv.DictWriter(output, COL_HEADERS)
     writer.writeheader()
 
-    regs = Registration.objects.select_related('embargo', 'retraction').all()
+    regs = Registration.objects.select_related('embargo', 'retraction', 'registration_approval').all()
 
     pbar = tqdm(total=regs.count())
 
@@ -37,6 +37,7 @@ def get_limbo_registrations():
             'date_created': reg.created,
             'date_registered': reg.registered_date,
             'moderation_state': reg.moderation_state,
+            'registration_approval_state': reg.registration_approval.state if reg.registration_approval else None,
             'embargo_state': reg.embargo.state if reg.embargo else None,
             'embargo_end_date': reg.embargo.end_date if reg.embargo else None,
             'retraction_state': reg.retraction.state if reg.retraction else None,
