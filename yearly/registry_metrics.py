77f50987f34a924d46bc10f3866c52b00420c86a
Krystal Hahn <krystalmhahn@gmail.com>
2026-07-14 14:58:13 -0700

    Parameterize functions to generate month ranges dynamically

--- yearly/registry_metrics.py
+++ yearly/registry_metrics.py
@@ -1,25 +1,35 @@
-def get_monthly_metrics():
+def get_monthly_registration_metrics(start_month, end_month):
     import csv
     import io
-    filename = '/tmp/monthly_COR_metrics.csv'
+    from django.utils import timezone
+    import pytz
+    from dateutil.relativedelta import relativedelta
+    from tqdm import tqdm
+
+    filename = '/tmp/monthly_registration_metrics.csv'
     COL_HEADERS = ['month', 'schema', 'accepted', 'embargoed', 'rejected', 'withdrawn', 'updated']
     output = io.StringIO()
     writer = csv.DictWriter(output, COL_HEADERS)
     writer.writeheader()
-    jan = timezone.datetime(2024,1,1,tzinfo=pytz.utc)
-    feb = timezone.datetime(2024,2,1,tzinfo=pytz.utc)
-    mar = timezone.datetime(2024,3,1,tzinfo=pytz.utc)
-    apr = timezone.datetime(2024,4,1,tzinfo=pytz.utc)
-    may = timezone.datetime(2024,5,1,tzinfo=pytz.utc)
-    jun = timezone.datetime(2024,6,1,tzinfo=pytz.utc)
-    jul = timezone.datetime(2024,7,1,tzinfo=pytz.utc)
-    dates = [jan, feb, mar, apr, may, jun, jul]
-    while len(dates) > 1:
-        start = dates.pop(0)
-        end = dates[0]
-        month = '-'.join(start.isoformat().split('-')[:2])
-        target_regs = Registration.objects.filter(provider__reviews_workflow='pre-moderation', created__gte=start, created__lt=end)
+
+    start_y, start_m = map(int, start_month.split("-"))
+    start = timezone.datetime(start_y, start_m, 1, tzinfo=pytz.utc)
+    end_y, end_m = map(int, end_month.split("-"))
+    end = timezone.datetime(end_y, end_m, 1, tzinfo=pytz.utc)
+
+    current = start
+
+    num_months = (end.year - start.year) * 12 + (end.month - start.month)
+    pbar = tqdm(total=num_months)
+    
+    while current < end:
+        next_month = current + relativedelta(months=1)
+
+        month = current.strftime("%b").lower()
+
+        target_regs = Registration.objects.filter(provider__reviews_workflow='pre-moderation', created__gte=current, created__lt=next_month)
         valid_schemas = set(list(target_regs.values_list('registered_schema__name', flat=True)))
+
         for sn in valid_schemas:
             writer.writerow({
                 'month': month,
@@ -31,33 +41,54 @@ def get_monthly_metrics():
                 'updated': target_regs.filter(registered_schema__name=sn).annotate(src=Count('schema_responses')).filter(src__gte=2).count()
 
             })
+        
+        pbar.update()
+        current = next_month
+            
+    pbar.close()
     with open(filename, 'w') as writeFile:
         writeFile.write(output.getvalue())
 
-def get_monthly_metrics_by_provider(targets=None):
+    print(f"CSV file saved to {filename}")
+
+def get_monthly_registry_metrics(start_month, end_month, targets=None):
     import csv
     import io
-    filename = '/tmp/monthly_COR_provider_metrics.csv'
-    COL_HEADERS = ['month', 'provider', 'schema', 'total', 'accepted', 'embargoed', 'rejected', 'withdrawn', 'updated']
+    from django.utils import timezone
+    import pytz
+    from dateutil.relativedelta import relativedelta
+    from tqdm import tqdm
+
+    filename = '/tmp/monthly_registry_metrics.csv'
+    COL_HEADERS = ['month', 'provider', 'schema', 'total', 'accepted', 'embargoed', 'embargo_approved', 'approved', 'rejected', 'withdrawn', 'updated']
     output = io.StringIO()
     writer = csv.DictWriter(output, COL_HEADERS)
     writer.writeheader()
-    jan = timezone.datetime(2024,1,1,tzinfo=pytz.utc)
-    feb = timezone.datetime(2024,2,1,tzinfo=pytz.utc)
-    mar = timezone.datetime(2024,3,1,tzinfo=pytz.utc)
-    apr = timezone.datetime(2024,4,1,tzinfo=pytz.utc)
-    may = timezone.datetime(2024,5,1,tzinfo=pytz.utc)
-    jun = timezone.datetime(2024,6,1,tzinfo=pytz.utc)
-    jul = timezone.datetime(2024,7,1,tzinfo=pytz.utc)
-    dates = [jan, feb, mar, apr, may, jun, jul]
-    while len(dates) > 1:
-        start = dates.pop(0)
-        end = dates[0]
-        month = '-'.join(start.isoformat().split('-')[:2])
-        target_providers = targets or RegistrationProvider.objects.exclude(_id='osf')
+
+    start_y, start_m = map(int, start_month.split("-"))
+    start = timezone.datetime(start_y, start_m, 1, tzinfo=pytz.utc)
+    end_y, end_m = map(int, end_month.split("-"))
+    end = timezone.datetime(end_y, end_m, 1, tzinfo=pytz.utc)
+
+    current = start
+
+    if targets:
+        target_providers = RegistrationProvider.objects.filter(_id__in=targets)
+    else: 
+        target_providers = RegistrationProvider.objects.all()
+
+    num_months = (end.year - start.year) * 12 + (end.month - start.month)
+    pbar = tqdm(total=num_months * target_providers.count())
+    
+    while current < end:
+        next_month = current + relativedelta(months=1)
+
+        month = current.strftime("%b").lower()
+
         for provider in target_providers:
-            target_regs = Registration.objects.filter(provider=provider, created__gte=start, created__lt=end)
+            target_regs = Registration.objects.filter(provider=provider, created__gte=current, created__lt=next_month)
             valid_schemas = set(list(provider.schemas.values_list('name', flat=True)))
+
             for sn in valid_schemas:
                 writer.writerow({
                     'month': month,
@@ -66,12 +97,20 @@ def get_monthly_metrics_by_provider(targets=None):
                     'total': target_regs.filter(registered_schema__name=sn).count(),
                     'accepted': target_regs.filter(registered_schema__name=sn, moderation_state='accepted').count(),
                     'embargoed': target_regs.filter(registered_schema__name=sn, moderation_state='embargo').count(),
+                    'embargo_approved': target_regs.filter(registered_schema__name=sn, embargo__state='approved').count(),
+                    'approved': target_regs.filter(registered_schema__name=sn, registration_approval__state='approved').count(),
                     'rejected': target_regs.filter(registered_schema__name=sn, moderation_state='rejected').count(),
                     'withdrawn': target_regs.filter(registered_schema__name=sn, moderation_state='withdrawn').count(),
                     'updated': target_regs.filter(registered_schema__name=sn).annotate(src=Count('schema_responses')).filter(src__gte=2).count()
-    
                 })
+
+            pbar.update()
+
+        current = next_month
+
+    pbar.close()
     with open(filename, 'w') as writeFile:
         writeFile.write(output.getvalue())
 
+    print(f"CSV file saved to {filename}")
 
