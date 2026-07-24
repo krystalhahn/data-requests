a7a8558b1480d4d14ecb9c093fde685743a07f88
Krystal Hahn <krystalmhahn@gmail.com>
2026-07-23 19:40:20 -0700

    Add version of script only pulling users if the email domain is detected in the primary email address

--- ad-hoc/users_from_email_domain.py
+++ ad-hoc/users_from_email_domain.py
@@ -36,3 +36,35 @@ def get_users_from_email_domain(descriptor, domains):
         writeFile.write(output.getvalue())
 
     print(f"CSV file saved to {filename}")
+
+def get_users_from_primary_email_domain(descriptor, domains):
+    import io
+    import csv
+    from tqdm import tqdm
+
+    filename = f'/tmp/{descriptor}_primary_email_domain_users.csv'
+    fieldnames = ['user_id', 'primary_email_address', 'all_emails']
+    output = io.StringIO()
+    writer = csv.DictWriter(output, fieldnames)
+    writer.writeheader()
+    
+    for domain in domains:
+        users = OSFUser.objects.filter(username__contains=domain).distinct()
+
+        pbar = tqdm(total = users.count())
+
+        for u in users:
+            writer.writerow({
+                'user_id': u._id,
+                'primary_email_address': u.username,
+                'all_emails': ', '.join(u.emails.values_list('address', flat=True))
+            })
+            pbar.update()
+
+        pbar.close()
+
+    pbar.close()
+    with open(filename, 'w') as writeFile:
+        writeFile.write(output.getvalue())
+
+    print(f"CSV file saved to {filename}")
