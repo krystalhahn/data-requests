c889062c2f20f1b7e419dc2c596bf01edadf0966
Krystal Hahn <krystalmhahn@gmail.com>
2026-07-27 15:20:52 -0700

    Consolidate function pulling from Email table and function pulling usernames not captured

--- ad-hoc/users_from_email_domain.py
+++ ad-hoc/users_from_email_domain.py
@@ -5,6 +5,7 @@ def get_users_from_email_domain(descriptor, domains):
     import io
     import csv
     from tqdm import tqdm
+    from django.db.models import Q
 
     filename = f'/tmp/{descriptor}_email_domain_users.csv'
     fieldnames = ['user_id', 'primary_email_address', 'all_emails']
@@ -12,61 +13,19 @@ def get_users_from_email_domain(descriptor, domains):
     writer = csv.DictWriter(output, fieldnames)
     writer.writeheader()
 
-    if len(domains) == 1:
-        emails = Email.objects.filter(address__contains=domains).select_related('user').distinct()
-    else:
-        email_query = Q()
-        for domain in domains:
-            email_query |= Q(address__icontains=domain)
+    query = Q()
+    for domain in domains:
+        query |= Q(emails__address__icontains=domain) | Q(username__icontains=domain)
 
-        emails = Email.objects.filter(email_query).select_related('user').distinct()
+    users = OSFUser.objects.filter(query).distinct().prefetch_related('emails')
 
-    user_ids = emails.values_list('user_id', flat=True).distinct()
-    users = OSFUser.objects.filter(id__in=user_ids)
-
-    pbar = tqdm(total = users.count())
-
-    for u in users:
+    for u in tqdm(users, total=users.count()):
         writer.writerow({
             'user_id': u._id,
             'primary_email_address': u.username,
             'all_emails': ', '.join(u.emails.values_list('address', flat=True))
         })
-        pbar.update()
 
-    pbar.close()
-    with open(filename, 'w') as writeFile:
-        writeFile.write(output.getvalue())
-
-    print(f"CSV file saved to {filename}")
-
-def get_users_from_primary_email_domain(descriptor, domains):
-    import io
-    import csv
-    from tqdm import tqdm
-
-    filename = f'/tmp/{descriptor}_primary_email_domain_users.csv'
-    fieldnames = ['user_id', 'primary_email_address', 'all_emails']
-    output = io.StringIO()
-    writer = csv.DictWriter(output, fieldnames)
-    writer.writeheader()
-    
-    for domain in domains:
-        users = OSFUser.objects.filter(username__contains=domain).distinct()
-
-        pbar = tqdm(total = users.count())
-
-        for u in users:
-            writer.writerow({
-                'user_id': u._id,
-                'primary_email_address': u.username,
-                'all_emails': ', '.join(u.emails.values_list('address', flat=True))
-            })
-            pbar.update()
-
-        pbar.close()
-
-    pbar.close()
     with open(filename, 'w') as writeFile:
         writeFile.write(output.getvalue())
 
