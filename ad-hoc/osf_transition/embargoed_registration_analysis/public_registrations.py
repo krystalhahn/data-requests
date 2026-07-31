ba1f12182a8052b4f3365a4a9a6c08a413e7b65a
Krystal Hahn <krystalmhahn@gmail.com>
2026-07-30 18:43:00 -0700

    Optimize script by precomputing creator stats

--- ad-hoc/osf_transition/embargoed_registration_analysis/public_registrations.py
+++ ad-hoc/osf_transition/embargoed_registration_analysis/public_registrations.py
@@ -1,52 +1,63 @@
 def get_public_registrations(start_month, end_month=None):
     import csv
-    import io
-    import datetime
-    from tqdm import tqdm
     import json
-    from django.utils import timezone
     import pytz
-    from dateutil.relativedelta import relativedelta
+    from django.db.models import Count, Prefetch
+    from django.utils import timezone
+    from tqdm import tqdm
 
-    filename = '/tmp/public_registrations.csv'
+    filename = "/tmp/public_registrations.csv"
     COL_HEADERS = ['reg_guid', 'is_public', 'is_deleted', 'embargo_state', 'reg_creator',
                    'date_created', 'date_registered',
                    'root_guid', 'date_root_created',
                    'registered_from_guid', 'date_registered_from_created',
                    'contributor_count', 'subject']
-    output = io.StringIO()
-    writer = csv.DictWriter(output, COL_HEADERS)
-    writer.writeheader()
 
-    start_y, start_m = map(int, start_month.split("-"))
-    start = timezone.datetime(start_y, start_m, 1, tzinfo=pytz.utc)
+    start_year, start_month_num = map(int, start_month.split("-"))
+    start = timezone.datetime(start_year, start_month_num, 1, tzinfo=pytz.UTC)
 
-    target_regs = Registration.objects.filter(created__gte=start, is_public=True)
+    queryset = (Registration.objects.filter(created__gte=start,is_public=True,)
+        .select_related("creator", "embargo", "root", "registered_from")
+        .prefetch_related(Prefetch("subjects"))
+        .annotate(contributor_count=Count("_contributors", distinct=True) )
+    )
 
     if end_month:
-        end_y, end_m = map(int, end_month.split("-"))
-        end = timezone.datetime(end_y, end_m, 1, tzinfo=pytz.utc)
+        end_year, end_month_num = map(int, end_month.split("-"))
+        end = timezone.datetime(end_year, end_month_num, 1, tzinfo=pytz.UTC)
+        queryset = queryset.filter(created__lt=end)
 
-        target_regs = Registration.objects.filter(created__gte=start, created__lt=end, is_public=True)
+    total = queryset.count()
 
-    for reg in tqdm(target_regs, total = target_regs.count()):
-        writer.writerow({
-            'reg_guid': reg._id,
-            'is_public': reg.is_public,
-            'is_deleted': True if reg.deleted else None,
-            'embargo_state': reg.embargo.state if reg.embargo else None,
-            'reg_creator': reg.creator._id,
-            'date_created': reg.created,
-            'date_registered': reg.registered_date,
-            'root_guid': reg.root._id if reg.root else None,
-            'date_root_created': reg.root.created if reg.root else None,
-            'registered_from_guid': reg.registered_from._id if reg.registered_from else None,
-            'date_registered_from_created': reg.registered_from.created if reg.registered_from else None,
-            'contributor_count': reg.contributors.count(),
-            'subject': json.dumps(list(reg.subjects.values_list('text', flat=True)) if hasattr(reg, 'subjects') else [])
-        })
+    with open(filename, "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=COL_HEADERS)
+        writer.writeheader()
 
-    with open(filename, 'w') as writeFile:
-        writeFile.write(output.getvalue())
+        for reg in tqdm(queryset.iterator(chunk_size=1000), total=total):
+            writer.writerow({
+                "reg_guid": reg._id,
+                "is_public": reg.is_public,
+                "is_deleted": bool(reg.deleted) or None,
+                "embargo_state": reg.embargo.state if reg.embargo else None,
+                "reg_creator": reg.creator._id,
+                "date_created": reg.created,
+                "date_registered": reg.registered_date,
+                "root_guid": reg.root._id if reg.root else None,
+                "date_root_created": reg.root.created if reg.root else None,
+                "registered_from_guid": (
+                    reg.registered_from._id
+                    if reg.registered_from
+                    else None
+                ),
+                "date_registered_from_created": (
+                    reg.registered_from.created
+                    if reg.registered_from
+                    else None
+                ),
+                "contributor_count": reg.contributor_count,
+                "subject": json.dumps(
+                    [subject.text for subject in reg.subjects.all()]
+                ),
+            })
 
     print(f"Output written to {filename}")
