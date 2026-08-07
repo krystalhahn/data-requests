63faf7d4b5a8e15a7c2f90ee08e50a3568857047
Krystal Hahn <krystalmhahn@gmail.com>
2026-08-06 17:41:00 -0700

    Consolidate functions to support configurable exclusion of nodes that were later made private

--- ad-hoc/osf_transition/weekly_active_users_nodes/weekly_nodes_made_public.py
+++ ad-hoc/osf_transition/weekly_active_users_nodes/weekly_nodes_made_public.py
@@ -1,5 +1,4 @@
-# specifically  "made_public" log is not followed by a "made_private" log
-def get_weekly_nodes_made_public(backup_cutoff):
+def get_weekly_nodes_made_public(backup_cutoff, exclude_later_made_private):
     import io
     import csv
     from tqdm import tqdm
@@ -24,82 +23,56 @@ def get_weekly_nodes_made_public(backup_cutoff):
     end = timezone.datetime(end_y, end_m, end_d, tzinfo=pytz.utc)
     start = end - datetime.timedelta(days=7)
 
-    actions = ["made_public", "made_private"]
+    # specifically  "made_public" log is not followed by a "made_private" log
+    if exclude_later_made_private:
+        actions = ["made_public", "made_private"]
 
-    logs = (
-        NodeLog.objects.filter(action__in=actions, created__gte=start, created__lt=end)
-        .select_related("node")
-        .order_by("node_id", "created")
-    )
+        logs = (
+            NodeLog.objects.filter(action__in=actions, created__gte=start, created__lt=end)
+            .select_related("node")
+            .order_by("node_id", "created")
+        )
 
-    node_data = {}
+        node_data = {}
 
-    for log in tqdm(logs, total=logs.count()):
-        if log.node is None:
-            continue
+        for log in tqdm(logs, total=logs.count()):
+            if log.node is None:
+                continue
 
-        data = node_data.setdefault(log.node_id, {
-            "node": log.node,
-            "has_made_public_log": False,
-            "date_made_public_log": None,
-        })
+            data = node_data.setdefault(log.node_id, {
+                "node": log.node,
+                "has_made_public_log": False,
+                "date_made_public_log": None,
+            })
 
-        if log.action == "made_public":
-            # latest state becomes public
-            data["has_made_public_log"] = True
-            data["date_made_public_log"] = log.created
+            if log.action == "made_public":
+                # latest state becomes public
+                data["has_made_public_log"] = True
+                data["date_made_public_log"] = log.created
 
-        elif log.action == "made_private":
-            # latest state becomes private
-            data["has_made_public_log"] = False
-            data["date_made_public_log"] = None
+            elif log.action == "made_private":
+                # latest state becomes private
+                data["has_made_public_log"] = False
+                data["date_made_public_log"] = None
 
-    for data in node_data.values():
-        if not data["has_made_public_log"]:
-            continue
+        for data in node_data.values():
+            if not data["has_made_public_log"]:
+                continue
 
-        node = data["node"]
+            node = data["node"]
 
-        writer.writerow({
-            "node_id": node._id,
-            "abstractnode_type": AbstractNode.objects.get(
-                guids___id=node._id
-            ).type,
-            "date_made_public_log": data["date_made_public_log"],
-            "has_made_public_log": True,
-        })
+            writer.writerow({
+                "node_id": node._id,
+                "abstractnode_type": AbstractNode.objects.get(
+                    guids___id=node._id
+                ).type,
+                "date_made_public_log": data["date_made_public_log"],
+                "has_made_public_log": True,
+            })
 
-    with open(filename, "w") as writeFile:
-        writeFile.write(output.getvalue())
-
-    print(f"Output written to {filename}")
-
-# regardless of whether there was a "made_private" log afterwards
-def get_weekly_nodes_made_public_all(backup_cutoff):
-    import io
-    import csv
-    from tqdm import tqdm
-    import datetime
-    import pytz
-    from django.utils import timezone
-
-    filename = '/tmp/weekly_nodes_made_public_all.csv'
-
-    fieldnames = [
-        'node_id',
-        'abstractnode_type',
-        'date_made_public_log'
-    ]
-
-    output = io.StringIO()
-    writer = csv.DictWriter(output, fieldnames)
-    writer.writeheader()
-
-    end_y, end_m, end_d = map(int, backup_cutoff.split("-"))
-    end = timezone.datetime(end_y, end_m, end_d, tzinfo=pytz.utc)
-    start = end - datetime.timedelta(days=7)
-
-    logs = (
+    # regardless of whether there was a "made_private" log afterwards
+    else:
+        logs = (
         NodeLog.objects.filter(action="made_public", created__gte=start, created__lt=end)
         .select_related("node")
         .order_by("node_id", "created")
@@ -119,7 +92,9 @@ def get_weekly_nodes_made_public_all(backup_cutoff):
                 guids___id=log.node._id
             ).type,
             "date_made_public_log": log.created,
+            "has_made_public_log": True
         })
+
 
     with open(filename, "w") as writeFile:
         writeFile.write(output.getvalue())
