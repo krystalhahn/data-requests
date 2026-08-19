e4e9d2f8b8276fc5de491f5436918412b3f743c4
Krystal Hahn <krystalmhahn@gmail.com>
2026-08-19 15:00:14 -0700

    Add progress bar

--- ad-hoc/osf_transition/transition_monitoring/download_events.py
+++ ad-hoc/osf_transition/transition_monitoring/download_events.py
@@ -1,37 +1,42 @@
 def get_download_events():
     import csv
+    from tqdm import tqdm
     from django.db import connection
 
     filename = "/tmp/download_events.csv"
 
     with connection.cursor() as cursor:
+
+        # get total number of download events for pbar
+        cursor.execute("""
+            SELECT COUNT(*)
+            FROM osf_downloadevent;
+        """)
+        total = cursor.fetchone()[0]
+
+        # get all download events
         cursor.execute("""
             SELECT *
             FROM osf_downloadevent
             ORDER BY id;
         """)
 
-        # get column names from the query
+        # get column names
         fieldnames = [column[0] for column in cursor.description]
 
         with open(filename, "w", newline="") as file:
             writer = csv.writer(file)
             writer.writerow(fieldnames)
 
-            processed = 0
+            # process rows in batches
+            with tqdm(total=total, desc="Writing download events") as pbar:
+                while True:
+                    rows = cursor.fetchmany(10000)
 
-            while True:
-                rows = cursor.fetchmany(10000)
+                    if not rows:
+                        break
 
-                if not rows:
-                    break
+                    writer.writerows(rows)
+                    pbar.update(len(rows))
 
-                writer.writerows(rows)
-                processed += len(rows)
-
-                print(
-                    f"Processed {processed:,} rows",
-                    end="\r"
-                )
-
-    print(f"\nOutput written to {filename}")
+    print(f"Output written to {filename}")
