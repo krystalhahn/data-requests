79abe2518c9b754f92f1af6dfcd6934b3e66a884
Krystal Hahn <krystalmhahn@gmail.com>
2026-08-26 14:25:31 -0700

    Expnad function to pull corresponding resource type to differentiate between downloads from nodes, registrations, and preprints

--- ad-hoc/osf_transition/transition_monitoring/download_events.py
+++ ad-hoc/osf_transition/transition_monitoring/download_events.py
@@ -7,6 +7,7 @@ def get_download_events():
     filename = "/tmp/download_events.csv"
 
     user_content_type_id = ContentType.objects.get_for_model(OSFUser).id
+    abstractnode_content_type_id = ContentType.objects.get_for_model(AbstractNode).id
 
     with connection.cursor() as cursor:
 
@@ -17,19 +18,36 @@ def get_download_events():
         """)
         total = cursor.fetchone()[0]
 
-        # get all download events and corresponding user GUID
+        # get all download events and corresponding user/resource information
         cursor.execute("""
             SELECT
                 de.*,
-                g._id AS user_guid
+                ug._id AS user_guid,
+                CASE
+                    WHEN an.id IS NOT NULL THEN an.type
+                    ELSE 'osf.preprint'
+                END AS resource_type
             FROM osf_downloadevent de
+
             LEFT JOIN osf_osfuser u
                 ON de.user_id = u.id
-            LEFT JOIN osf_guid g
-                ON u.id = g.object_id
-                AND g.content_type_id = %s
+
+            LEFT JOIN osf_guid ug
+                ON u.id = ug.object_id
+                AND ug.content_type_id = %s
+
+            LEFT JOIN osf_guid rg
+                ON de.resource_guid = rg._id
+                AND rg.content_type_id = %s
+
+            LEFT JOIN osf_abstractnode an
+                ON rg.object_id = an.id
+
             ORDER BY de.id;
-        """, [user_content_type_id])
+        """, [
+            user_content_type_id,
+            abstractnode_content_type_id,
+        ])
 
         # get column names
         fieldnames = [column[0] for column in cursor.description]
