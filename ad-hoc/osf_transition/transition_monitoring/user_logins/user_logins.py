300fcd5c8d274e07bd1a0e641bb2b7c8448c2856
Krystal Hahn <krystalmhahn@gmail.com>
2026-09-25 13:04:26 -0700

    Add function counting user logins by admin status and project count

--- ad-hoc/osf_transition/transition_monitoring/user_logins/user_logins.py
+++ ad-hoc/osf_transition/transition_monitoring/user_logins/user_logins.py
@@ -57,3 +57,71 @@ def get_total_users_since_transition(backup_cutoff):
     users_more_than_5_private_projects = total_logged_in_users.filter(private_project_count__gt=5).count()
     print(f"Users with >5 projects: {users_more_than_5_projects}")
     print(f"Users with >5 private projects: {users_more_than_5_private_projects} (took {time.time() - t0:.2f}s)")
+
+def get_admin_users_since_transition(backup_cutoff):
+    import datetime
+    import pytz
+    from django.utils import timezone
+    import time
+    from django.db.models import Count, Q, Subquery, Value, CharField
+    from django.db.models.functions import Cast, Concat
+    from django.db.models import CharField
+
+    end_y, end_m, end_d = map(int, backup_cutoff.split("-"))
+    end = timezone.datetime(end_y, end_m, end_d, tzinfo=pytz.utc)
+    start = timezone.datetime(2026, 8, 9, tzinfo=pytz.utc)
+
+    print("Counting admin users by project count...")
+    t0 = time.time()
+    admin_users = OSFUser.objects.filter(
+        date_last_login__gte=start,
+        date_last_login__lt=end,
+        groups__name__startswith="node_",
+        groups__name__endswith="_admin"
+    ).annotate(
+        admin_project_count=Count(
+            "groups",
+            filter=Q(
+                groups__name__startswith="node_",
+                groups__name__endswith="_admin"
+            ),
+            distinct=True
+        )
+    )
+
+    users_any_admin_projects = admin_users.count()
+    users_more_than_5_admin_projects = admin_users.filter( admin_project_count__gt=5).count()
+    print(f"Users with >0 admin projects: {users_any_admin_projects}")
+    print(f"Users with >5 admin projects: {users_more_than_5_admin_projects} (took {time.time() - t0:.2f}s)")
+
+    print("Counting admin users by private project count...")
+    t0 = time.time()
+    private_admin_group_names = Node.objects.filter(
+        type="osf.node",
+        is_public=False
+    ).annotate(
+        admin_group_name=Concat(
+            Value("node_"),
+            Cast("id", CharField()),
+            Value("_admin")
+        )
+    ).values("admin_group_name")
+
+    private_admin_users = OSFUser.objects.filter(
+        date_last_login__gte=start,
+        date_last_login__lt=end,
+        groups__name__in=Subquery(private_admin_group_names)
+    ).annotate(
+        private_admin_project_count=Count(
+            "groups",
+            filter=Q(
+                groups__name__in=Subquery(private_admin_group_names)
+            ),
+            distinct=True
+        )
+    )
+
+    users_any_private_admin_projects = private_admin_users.count()
+    users_more_than_5_private_admin_projects = private_admin_users.filter(private_admin_project_count__gt=5).count()
+    print(f"Users with >0 private admin projects: {users_any_private_admin_projects}")
+    print(f"Users with >5 private admin projects: {users_more_than_5_private_admin_projects} (took {time.time() - t0:.2f}s)")
